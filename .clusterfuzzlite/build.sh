2dbd32dd2b81c16da5c39413d6a9e89ec450c77d
ssakpal2 <104564045+ssakpal2@users.noreply.github.com>
2024-06-16 15:29:21 +0200

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -22,5 +22,5 @@ make -j$(nproc) all    # Build the fuzz targets.
 
 # Copy the fuzzer executables, zip-ed corpora, option and dictionary files to $OUT
 find . -name '*_fuzzer' -exec cp -v '{}' $OUT ';'
-find . -name '*_fuzzer.dict' -exec cp -v '{}' $OUT ';'     # If you have dictionaries.
-find . -name '*_fuzzer.options' -exec cp -v '{}' $OUT ';'  # If you have custom options.
+#find . -name '*_fuzzer.dict' -exec cp -v '{}' $OUT ';'     # If you have dictionaries.
+#find . -name '*_fuzzer.options' -exec cp -v '{}' $OUT ';'  # If you have custom options.
